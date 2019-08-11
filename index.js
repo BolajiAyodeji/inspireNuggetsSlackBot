3e99a8447eb7d0a9e5f5a67d23234afc60fd15bd
bolajiayodeji <sirbeejay1@gmail.com>
2019-08-11 22:59:07 +0100

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+const slackBot = require('slackbots');
+const axios = require('axios');
+
+const bot = new slackBot({
+    token: 'xoxb-407013445267-724945161175-ZI0AHUfmfVO2K0Ra1Xx6IZ35'
+    name: 'inspire Nuggets'
+})
