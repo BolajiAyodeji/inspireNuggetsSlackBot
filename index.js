a2257ad24ecd95844779e01e8161c78e22b9b33c
bolajiayodeji <sirbeejay1@gmail.com>
2019-08-11 23:20:17 +0100

    setup nodemon

--- index.js
+++ index.js
@@ -2,6 +2,32 @@ const slackBot = require('slackbots');
 const axios = require('axios');
 
 const bot = new slackBot({
-    token: 'xoxb-407013445267-724945161175-ZI0AHUfmfVO2K0Ra1Xx6IZ35',
-    name: 'inspire Nuggets'
+    token: 'xoxb-407013445267-724945161175-uGx7WM8RTqY2aVRKd8fNcewW',
+    name: 'inspireNuggets'
+})
+
+// Start Handler
+bot.on('start', () => {
+    const params = {
+        icon_emoji: ':carlton:'
+    }
+
+    bot.postMessageToChannel(
+        'random',
+        'Get inspired while working with @inspireNuggets!',
+        params
+    );
+})
+
+// Error Handler
+bot.on('eror', (err) => {
+    console.log(err);
+})
+
+// Message Handler
+bot.on('message', (data) => {
+    if(data.type !== 'message') {
+        return;
+    }
+    console.log(data);
 })
