30c67a73a54461c527f44e5c470a8c341266c962
jacksonsouza <jsouza@avvo.com>
2020-10-01 21:15:29 -0700

    rotation command finished

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 npm start -> node app.js (Express server)
 
 ngrok for development - so that slack can "call" the local service, not at localhost
-ngrok http 3000 + npm start
+./ngrok http 3000 + npm start
--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,8 @@ router.get('/', function(req, res, next) {
 
 var commands = require('../lib/commands')
 
-router.post('/commands/list', function(req, res, next) {
-  res.send(commands.list())
-});
-router.post('/commands/add', function(req, res, next) {
-  res.send(commands.add(req))
-});
-router.post('/commands/generate', function(req, res, next) {
-  res.send(commands.generate(req))
+router.post('/commands/generate', async function(req, res, next) {
+  res.send(await commands.generate(req))
 });
 
 module.exports = router;
